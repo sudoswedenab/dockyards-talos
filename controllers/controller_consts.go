983bfd7fc465d45a9989c987104379e23eeb4d55
Alexander Backlund <alexander.backlund@sudosweden.com>
2025-02-10 15:15:38 +0100

    Add missing controller constants

---
+++ controllers/controller_consts.go
@@ -0,0 +1,11 @@
+package controllers
+
+const (
+	AnnotationTalosPlatformName = "talos.dockyards.io/platform-name"
+	AnnotationTalosSchematicID  = "talos.dockyards.io/schematic-id"
+)
+
+const (
+	// Default image factory schematic id without any customizations
+	DefaultSchematicID = "376567988ad370138ad8b2698212367b8edcb69b5fd68c80be1f2ec7d603b4ba"
+)
