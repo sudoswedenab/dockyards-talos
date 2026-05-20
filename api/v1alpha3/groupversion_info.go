d9262c2d353f1852f0a8b5f15269c874900e3a49
Maxim Kravchenko <maxim.kravchenko@sudosweden.com>
2026-05-20 12:55:04 +0200

    move linkconfig API to talos group

--- api/v1alpha3/groupversion_info.go
+++ api/v1alpha3/groupversion_info.go
@@ -1,14 +1,14 @@
 package v1alpha3
 
 // +kubebuilder:object:generate=true
-// +groupName=dockyards.io
+// +groupName=talos.dockyards.io
 
 import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/scheme"
 )
 
-const GroupName = "dockyards.io"
+const GroupName = "talos.dockyards.io"
 
 var (
 	SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: "v1alpha3"}
